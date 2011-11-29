c995d88809a5317bb29b8464cec987e5ed364590
Riccardo GitHubbi <rusko@palladius.it>
2011-11-29 11:48:00 +0000

    tmp

--- color_matrix.rb
+++ color_matrix.rb
@@ -1,8 +1,8 @@
 =begin
 
-  implements a matrix concept, extending ruby core Matrix:
-  
-  http://corelib.rubyonrails.org/classes/Matrix.html
+  implements a matrix concept, extending ruby core Array.
+
+	Implements all the functions required by the exercise
 
 =end
 
@@ -12,6 +12,7 @@ class ColorMatrix < Array
   @@version  = "0.2" 
   @@colors = {
     :white  => 'O',
+    :nil    => '.', # for labelling, private value
   }
 	@@debug = true
   attr_accessor :cols, :rows
@@ -30,12 +31,14 @@ class ColorMatrix < Array
   # P(x,y) in human/math notation corresponds to
   # x-1 for X and y'1 for Y in matrix notation.
   # which is (x-1)+(y-1)*NUM_COLUMNS in streteched array notation
-  def colour(x,y,color)
+	# SETS COLOUR
+  def set_colour(x,y,color)
+		deb "set(#{x},#{y},'#{color}')"
     self[(x-1) + (y-1) * cols ] = ColorMatrix.smart_color( color )
   end
   
   # returns element(X,Y) in the array
-  def get(x,y)
+  def get_colour(x,y)
     self[(x-1) + (y-1) * cols ]
   end
   
@@ -52,16 +55,102 @@ class ColorMatrix < Array
     validate rescue false
   end
 
-	def	draw_vertical(x,y1,y2,color)
-		:TODO
-	end
-	def	draw_horizontal(x1,x2,y,color)
-		:TODO
-	end
+  def draw_vertical(x,y1,y2,color)
+    (y1..y2).each{|y| set(x,y,color) }
+  end  
+  
+  def draw_horizontal(x1,x2,y,color)
+    (x1..x2).each{|x| set(x,y,color) }
+  end
+  
+  # 4-connectivity neighbours are: N and W
+  # 8-connectivity neihbours are: W, NW, N, NE
+  def half_neighbours_of(x,y)
+    neighbours = []
+    neighbours << [x-1,y] unless x<2 # west unless boundary
+    neighbours << [x,y-1] unless y<2 # north unless boundary
+    neighbours
+  end
+  
+  def neighbours_colors_of(x,y)
+    half_neighbours_of(x,y).map{|el| get(el[0],el[1]) }                    
+  end
 
-	def fill(x,y,color)
+  def west?(x,y)
+    x > 1
+  end
+  def west(x,y)
+    [x-1,y] if west?
+  end
+  
 
+=begin
+	Fill the region R with the colour C. 
+	R is defined as: Pixel (X,Y) belongs to R. 
+	Any other pixel which is the same colour as (X,Y) and shares a common side with any pixel in R also belongs to this region.
+
+	From the filling description looks like a 4-connectivity filling (I did on my thesis!)
+
+	The solution consistes on Labelling, then filling everything which ahs the same label as the P(x,y)
+
+	Pass 1:
+		Create a matrix of boolean initialiazed to false. I reuse my matrix with true (1) and false (0)
+=end
+  
+  
+  def twopass(data)
+    linked = []
+    labels = ColorMatrix.new(rows,cols,:background)
+    next_label = 0
+    
+    # First pass
+    (1..rows).each do |x|
+      (1..cols).each do |y|
+        deb "P(#{x};#{y}) "
+        if data.get(x,y) != :background
+	  neighbour_labels = neighbours_colors_of(x,y)
+	  if neighbour_labels.size == 0 
+	    deb "empty neighbours!"
+	    linked[next_label] = true
+	    labels.set(x,y,next_label) 
+	    next_label = next_label + 1       
+	  else # not empty
+	    # find the smallest label
+	    labels.set(x,y,neighbour_labels.min)
+	    for label in neighbour_labels do
+	      linked[label] = union(linked[label],neighbour_labels)
+	    end
+	  end
 	end
+      end
+    end
+    
+    
+  end
+
+  def fill(x,y,color)
+    #set(x,y,color)
+    deb "Original matrix: \n#{self}"
+    f = twopass(self)
+    # First I create a labelling matrix m2
+    m2 = ColorMatrix.new(self.rows,self.cols,:nil)
+    #m2.set(x,y,color)
+    # first pass
+    labeln = 0
+    (1..rows).each do |x|
+      (1..cols).each do |y|
+        
+        #deb  "#{x},#{y}"  
+        # does the pixel west have
+      end
+    end
+    deb "Labelling matrix: \n#{m2}"
+    # west
+    neighbours = half_neighbours_of(x,y)
+    puts "neighbours = #{neighbours}"
+    
+    
+  end
   
   def see
     deb "[SEE] This matrix has the following elements:\n"
@@ -78,8 +167,11 @@ class ColorMatrix < Array
   alias :S :see
 	alias :V :draw_vertical
   alias :X :terminate
-  alias :L :colour
+  alias :L :set_colour
   alias :I :initialize
+  alias :set    :set_colour
+  alias :colour :set_colour
+  alias :get :get_colour
 
 public
 
--- main.rb
+++ main.rb
@@ -1,24 +1,32 @@
 #!/usr/bin/env ruby
 
+=begin
+
+	This class just calls the sample ColorMatrix class
+	as a proof of concept. The parsing can be done in a second time 
+
+=end
+
 # TODO better inclusion, not tree-dependant :)
 require '~/git/colormatrix/color_matrix.rb'
 
 def main
-  puts 'Hello from main'
-  puts '---------------'
   puts "\n1. Creating matrix"
   matrix = ColorMatrix.new(5,6)
-  #p "DEB class=#{matrix.class}"
   puts "\n2. Colouring:"
   matrix.L( 2,3,'A')
+  puts "\n3. Filling:"
   matrix.F(3,3,'J')
   matrix.V(2,3,4,'W') 
   matrix.H(3,4,2,'Z')
-  matrix.S
-  
   puts "\n3. Printing"
+  # To test the filling:
+	  matrix.V(5,2,4,'S') 
+	  matrix.H(3,4,5,'N')
+	  matrix.F(3,4,'i')
+	  matrix.F(1,2,'o')
   matrix.S()
+  #sleep 1
 end
 
-
-main
+main()
